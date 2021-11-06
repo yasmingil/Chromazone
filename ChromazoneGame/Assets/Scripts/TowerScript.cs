b8e3c6f34a763c997e13e2772d5d2a8f7f191d8f
lolkat777 <nchan103@usc.edu>
2021-11-06 00:38:22 -0700

    work on towerscript

--- ChromazoneGame/Assets/Scripts/TowerScript.cs
+++ ChromazoneGame/Assets/Scripts/TowerScript.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class TowerScript : MonoBehaviour
 {
     [SerializeField] private int Health;
+    [SerializeField] private float range;
     [SerializeField] private Color towerColor;
     [SerializeField] private float attackSpeed;
+    [SerializeField] private int dammage;
     [SerializeField] private float bulletSpeed;
     [SerializeField] private enum shotType { SPREAD, SINGLE }; 
 
@@ -19,6 +21,17 @@ public class TowerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject closestEnemy;
+        float closestDist;
+        foreach(var g in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, g.transform.position);
+            if (distance <= range)
+            {
+
+            }
+
+        }
     }
 }
