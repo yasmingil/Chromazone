f1ab5455605bd046ec2ff5dbc12969e9f7bea77a
virgilian <vecollin@gmail.com>
2021-11-06 13:20:31 -0700

    Added Player Prefab, continuing to work on

--- ChromazoneGame/Assets/Scripts/TowerPlacement.cs
+++ ChromazoneGame/Assets/Scripts/TowerPlacement.cs
@@ -15,16 +15,28 @@ public class TowerPlacement : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            
+            PlaceTower(tower1);
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            PlaceTower(tower2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            PlaceTower(tower3);
         }
     }
 
     private void PlaceTower(GameObject tower)
     { 
         GameObject[] towers = GameObject.FindGameObjectsWithTag("Tower");
+        float closestDist = float.MaxValue;
         foreach (var t in towers)
         {
-            
+            if (Vector3.Distance(t.transform.position, transform.position) <= closestDist)
+            {
+                
+            }
         }
     }
 }
