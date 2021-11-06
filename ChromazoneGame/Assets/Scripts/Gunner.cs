031bb9e74de2472458afef69bd466a0504c784dc
Yasmin Gil <yasmingi@usc.edu>
2021-11-06 13:24:27 -0700

    Updated Gunner Script
    Bullets shooting at player

--- ChromazoneGame/Assets/Scripts/Gunner.cs
+++ ChromazoneGame/Assets/Scripts/Gunner.cs
@@ -6,6 +6,8 @@ public class Gunner : MonoBehaviour
 {
     [SerializeField] private int damage;
     [SerializeField] private float bulletSpeed;
+    [SerializeField] private float bulletCooldown;
+    private float counter = 0f;
     [SerializeField] private enum shotType { SPREAD, SINGLE };
     [SerializeField] private GameObject bullet;
     // Start is called before the first frame update
@@ -17,10 +19,24 @@ public class Gunner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // TODO:
-        // Get list of gameobjects that are towers and player. 
-        // take priority of which one is closer, so if player is out of certain range,
-        // the gunner will focus bullets on tower
+        // constant shooting of bullets
+        if (counter <= bulletCooldown)
+        {
+            counter += Time.deltaTime;
+        }
+        else
+        {
+            counter = 0;
+            GameObject spawnBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+            // Get player position
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            var direction = player.transform.position - transform.position;
+            direction.z = 0;
+            direction.Normalize();
+            var velocity = direction * bulletSpeed;
+            spawnBullet.GetComponent<Rigidbody2D>().velocity = velocity;
+        }
+        
 
         // TODO:
         // limit range on gunners? (not shooting from across the map)
