07c85b1433dc89ce1f0915f322b7c863e5a1ed0f
Ryan Lau <rclau@usc.edu>
2021-11-07 11:00:14 -0800

    victory/defeat scenes
    add scenes for winning and losing the game

---
+++ ChromazoneGame/Assets/Scripts/VictoryScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryScript : MonoBehaviour
+{
+    // public GameObject MainMenu;
+    // public GameObject CreditsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PlayAgainButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("InsertNameofFirstScene");
+    }
+
+    public void MainMenuButton()
+    {
+        // Show Main Menu
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+    }
+}
