67ee71f248c53c58221b2d8efedb9f6746ccf30f
lolkat777 <nchan103@usc.edu>
2021-11-06 19:09:56 -0700

    tower hp bar

--- ChromazoneGame/Assets/Scripts/TowerScript.cs
+++ ChromazoneGame/Assets/Scripts/TowerScript.cs
@@ -75,8 +75,8 @@ public class TowerScript : MonoBehaviour
         //make towerhealthindi be proportional to health
 
         GameObject canvas = gameObject.transform.GetChild(1).gameObject;
-        GameObject HPBar = canvas.transform.GetChild(1).gameObject;
-        HPBar.GetComponent<Image>().fillAmount = (Health / maxHealth);
+        GameObject HPBar = canvas.transform.GetChild(0).gameObject;
+        HPBar.GetComponent<Image>().fillAmount = Health/maxHealth;
 
     }
 
