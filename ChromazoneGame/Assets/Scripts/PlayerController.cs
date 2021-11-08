7af2fadc71c7ecdd1c83dc2f8f138049900255a2
ZachNorthrop <znorthro@usc.edu>
2021-11-07 16:26:32 -0800

    push

--- ChromazoneGame/Assets/Scripts/PlayerController.cs
+++ ChromazoneGame/Assets/Scripts/PlayerController.cs
@@ -39,6 +39,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         radiusUI.enabled = false;
         placementUI.enabled = false;
+        currentState = playerState.SHOOTING;
     }
 
     private void Update()
